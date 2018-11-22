1a67c523c6ccd0602ddac0a0366dd81a1ec993ba
Qorridor <coendezeeuw@icloud.com>
2018-11-22 15:35:50 +0100

    Update accordion.js

--- accordion.js
+++ accordion.js
@@ -1,24 +1,23 @@
-let accordionCollection = document.getElementsByClassName('accordion__trigger');
-let accordionItems = Array.from(accordionCollection);
-let accordionTransition = 'transition: all .6s cubic-bezier(.5, .5, 0, 1)';
-
-accordionItems.forEach(function(accordionItem) {
-    accordionItem.addEventListener('click', function() {
-    let child = accordionItem.parentNode.querySelector('.accordion__content');
-        if( child.style.height.indexOf('%') === -1 ) {
-            accordionItem.parentNode.classList.add('is-open');
-            child.style = 'height: '+child.scrollHeight+'px; '+accordionTransition+'';
+var unfoldSelector = document.getElementsByClassName('accordion__trigger');
+Array.prototype.forEach.call(unfoldSelector, function(unfoldItem) {
+    unfoldItem.addEventListener('click', function() {
+        var child = unfoldItem.parentNode.querySelector('.accordion__content');
+        if (child.style.height.indexOf('%') === -1) {
+            unfoldItem.parentNode.classList.add('is-open');
+            child.style.height = child.scrollHeight+'px';
         } else {
-            accordionItem.parentNode.classList.remove('is-open');
-            child.style = 'height: '+child.scrollHeight+'px; transition: none;';
-            setTimeout(function() {
-               child.style = 'height: 0; '+accordionTransition+';';
+            unfoldItem.parentNode.classList.remove('is-open');
+            child.style.transition = 'none';
+            child.style.height = child.scrollHeight+'px';
+            setTimeout(function () {
+                child.style.transition = null;
+                child.style.height = '0px';
             }, 1);
         }
 
         child.addEventListener('transitionend', function () {
-            if( child.style.height !== '0px' && child.offsetHeight !== 0 ) {
-                child.style = 'height: 100%;';
+            if (child.style.height !== '0px' && child.offsetHeight !== 0) {
+                child.style.height = '100%';
             } else {
                 child.removeAttribute('style');
             }

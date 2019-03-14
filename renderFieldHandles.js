d05f47a14709559f02180354449fc246d107dbb8
Qorridor <coendezeeuw@icloud.com>
2019-03-14 23:41:23 +0100

    Update renderFieldHandles.js

--- renderFieldHandles.js
+++ renderFieldHandles.js
@@ -2,119 +2,131 @@ var fields = document.querySelectorAll('.fld-field');
 var fieldHeadings = document.querySelectorAll('#fields .field:not(#title-field) .heading');
 
 function createFieldStorage() {
-    var fields = document.querySelectorAll('#fields tbody tr');
-    if (fields.length && !window.localStorage.getItem('fieldData')) {
-        var fieldData = [];
-        fields.forEach(function(field) {
-            var handle = field.querySelector('code').textContent;
-            var id = field.getAttribute('data-id');
-            fieldData.push([handle, id]);
-        });
-
-        if (fieldData) {
-            localStorage.setItem('fieldData', JSON.stringify(fieldData));
-        }
-    }
+	var fields = document.querySelectorAll('#fields tbody tr');
+	if( fields.length && ! window.localStorage.getItem('fieldData') ) {
+		var fieldData = [];
+		fields.forEach(function(field) {
+			var handle = field.querySelector('code').textContent;
+			var id = field.getAttribute('data-id');
+			fieldData.push([handle, id]);
+		});
+		
+		if( fieldData ) { 
+			localStorage.setItem('fieldData', JSON.stringify(fieldData));
+		}
+	}
 }
 
 createFieldStorage();
 
 function fetchFieldStorage() {
-    var fieldStorage = localStorage.getItem('fieldData');
-    if (fieldStorage) {
-        var parsedFieldStorage = JSON.parse(fieldStorage);
-        var handles = document.querySelectorAll('.fieldHandle');
-
-        parsedFieldStorage.forEach(function(parsedField) {
-            handles.forEach(function(handle) {
-                if (parsedField[0] == handle.value) {
-                    var fieldLink = '<a class="handleIcon" data-icon="settings" href="' + window.Craft.baseCpUrl + '/settings/fields/edit/' + parsedField[1] + '" target="_blank"></a>';
-                    console.log(handle.parentNode.querySelector('legend'));
-                    handle.parentNode.querySelector('legend').insertAdjacentHTML('beforeEnd', fieldLink);
-                }
-            });
-        });
-    }
+	var fieldStorage = localStorage.getItem('fieldData');
+	if( fieldStorage ) {
+		var parsedFieldStorage = JSON.parse(fieldStorage);
+		var handles = document.querySelectorAll('.fieldHandle');
+		
+		parsedFieldStorage.forEach(function(parsedField) {
+			handles.forEach(function(handle) {
+				if( parsedField[0] == handle.value ) {
+					var fieldLink = '<a class="handleIcon" data-icon="settings" href="'+window.Craft.baseCpUrl+'/settings/fields/edit/'+parsedField[1]+'" target="_blank"></a>';
+					console.log(handle.parentNode.querySelector('legend'));
+					handle.parentNode.querySelector('legend').insertAdjacentHTML('beforeEnd', fieldLink);
+		        }
+		    });
+		});
+	}
 }
 
 function renderSectionHandles() {
-    fields.forEach(function(field) {
-        var field = field.childNodes;
-        field.forEach(function(child) {
-            if (child.tagName === "SPAN") {
-                var fieldHandle = child.getAttribute("title");
-                child.parentNode.insertAdjacentHTML('beforeEnd', renderHandleHTML(fieldHandle));
-
-            }
-        });
-    });
-
-    document.querySelectorAll('.fld-tab').forEach(function(tab) {
-        tab.style.cssText = "height: auto; width: auto;";
-    });
+  fields.forEach(function(field) {
+      var field = field.childNodes;
+      field.forEach(function(child) {
+          if( child.tagName === "SPAN" ) {
+              var fieldHandle = child.getAttribute("title");
+              child.parentNode.insertAdjacentHTML('beforeEnd', renderHandleHTML(fieldHandle));
+              
+          }
+      });
+  });
+  
+  document.querySelectorAll('.fld-tab').forEach(function(tab) {
+      tab.style.cssText = "height: auto; width: auto;";
+  });
 }
 
 function renderEntryHandles() {
     fieldHeadings.forEach(function(fieldHeading) {
         var fieldNodes = fieldHeading.childNodes;
         fieldNodes.forEach(function(fieldNode) {
-            if (fieldNode.tagName == "LABEL") {
+            if( fieldNode.tagName == "LABEL" ) {
                 var fieldHandle = fieldNode.getAttribute("for").split('-').pop();
                 fieldNode.parentNode.insertAdjacentHTML('beforeEnd', renderHandleHTML(fieldHandle));
-
-            }
-        });
+                
+            } 
+        }); 
     });
 }
 
 function copyHandles() {
-    var legendLabel = "Handle";
-    var fieldHandles = document.querySelectorAll('.fieldHandle');
-
-    fieldHandles.forEach(function(fieldHandle) {
-        fieldHandle.addEventListener('click', function(e) {
-            var legend = fieldHandle.parentNode.querySelector('.legendLabel');
-            var legendText = legend.textContent;
-
-            fieldHandle.select();
-            document.execCommand('copy');
-            legend.textContent = "Copied";
-
-            setTimeout(function() {
-                legend.textContent = legendLabel;
-            }, 300);
-        });
-    });
+	var legendLabel = "Handle";
+  var fieldHandles = document.querySelectorAll('.fieldHandle');
+	
+	fieldHandles.forEach(function(fieldHandle) {
+	    fieldHandle.addEventListener('click', function(e) {
+				var legend = fieldHandle.parentNode.querySelector('.legendLabel');
+				var legendText = legend.textContent;
+				
+				fieldHandle.select();
+				document.execCommand('copy');
+				legend.textContent = "Copied";
+				
+				setTimeout(function() {
+					legend.textContent = legendLabel;
+				}, 300);
+	    });
+	});
 }
 
 function renderHandleHTML(value) {
-    return '<fieldset class="fieldset text"><legend><span class="legendLabel">Handle</span></legend><input class="fieldHandle" value="' + value + '" title="Click to copy" readonly="readonly"></fieldset>';
+	return '<fieldset class="fieldset text"><legend><span class="legendLabel">Handle</span></legend><input class="fieldHandle" value="'+value+'" title="Click to copy" readonly="readonly"></fieldset>';
 }
+
+var buttonLabel = null;
+function setHandleButtonLabel() {
+	if( window.localStorage.getItem('cpShowHandles') ) {
+		document.body.classList.add('showHandles');
+		buttonLabel = "Hide handles";
+	} else {
+		buttonLabel = "Show handles";
+	}
+}
+
+setHandleButtonLabel();
 
 function renderHandleButton() {
-    var buttonVar = '<div class="handleButton btn submit">Handles</div>';
-    document.querySelector('#crumbs').insertAdjacentHTML('beforeEnd', buttonVar);
-
-    if (window.localStorage.getItem('cpShowHandles')) {
-        document.body.classList.add('showHandles');
-    }
-
-    var showButton = document.querySelector('.handleButton');
-    showButton.addEventListener('click', function(e) {
-        document.body.classList.toggle('showHandles');
-        if (!window.localStorage.getItem('cpShowHandles')) {
-            localStorage.setItem('cpShowHandles', true);
-        } else {
-            localStorage.removeItem('cpShowHandles');
-        }
-    });
+	
+	var buttonVar = '<div class="handleButton btn submit">'+buttonLabel+'</div>';
+	document.querySelector('#crumbs').insertAdjacentHTML('beforeEnd', buttonVar);
+	
+	var showButton = document.querySelector('.handleButton');
+	
+	showButton.addEventListener('click', function() {
+		document.body.classList.toggle('showHandles');
+		this.textContent = buttonLabel;
+		if( ! window.localStorage.getItem('cpShowHandles') ) {
+			localStorage.setItem('cpShowHandles', true);
+		} else {
+			localStorage.removeItem('cpShowHandles');
+		}
+		setHandleButtonLabel();
+	});
 }
 
-if (fields.length > 0 || fieldHeadings.length > 0) {
-    renderHandleButton();
-    renderEntryHandles();
-    renderSectionHandles();
-    copyHandles();
-
-    window.onload = fetchFieldStorage();
+if( fields.length > 0 || fieldHeadings.length > 0 ){
+	renderHandleButton();
+	renderEntryHandles();
+	renderSectionHandles();
+	copyHandles();
+	
+	window.onload = fetchFieldStorage();
 }

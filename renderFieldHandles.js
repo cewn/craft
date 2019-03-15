ea27530919201a552683008e6a8c416080177987
Qorridor <coendezeeuw@icloud.com>
2019-03-15 09:37:18 +0100

    Update renderFieldHandles.js

--- renderFieldHandles.js
+++ renderFieldHandles.js
@@ -2,17 +2,19 @@ var fields = document.querySelectorAll('.fld-field');
 var fieldHeadings = document.querySelectorAll('#fields .field:not(#title-field) .heading');
 
 function createFieldStorage() {
-	var fields = document.querySelectorAll('#fields tbody tr');
-	if( fields.length && ! window.localStorage.getItem('fieldData') ) {
-		var fieldData = [];
-		fields.forEach(function(field) {
-			var handle = field.querySelector('code').textContent;
-			var id = field.getAttribute('data-id');
-			fieldData.push([handle, id]);
-		});
-		
-		if( fieldData ) { 
-			localStorage.setItem('fieldData', JSON.stringify(fieldData));
+	if( window.location.href === window.Craft.baseCpUrl+'/settings/fields' ) {
+		var fields = document.querySelectorAll('#fields tbody tr');
+		if( fields.length ) {
+			var fieldData = [];
+			fields.forEach(function(field) {
+				var handle = field.querySelector('code').textContent;
+				var id = field.getAttribute('data-id');
+				fieldData.push([handle, id]);
+			});
+			
+			if( fieldData ) { 
+				localStorage.setItem('fieldData', JSON.stringify(fieldData));
+			}
 		}
 	}
 }
@@ -50,7 +52,7 @@ function renderSectionHandles() {
   });
   
   document.querySelectorAll('.fld-tab').forEach(function(tab) {
-      tab.style.cssText = "height: auto; width: auto;";
+      tab.style.cssText = "height: auto;";
   });
 }
 
@@ -91,34 +93,24 @@ function renderHandleHTML(value) {
 	return '<fieldset class="fieldset text"><legend><span class="legendLabel">Handle</span></legend><input class="fieldHandle" value="'+value+'" title="Click to copy" readonly="readonly"></fieldset>';
 }
 
-var buttonLabel = null;
-function setHandleButtonLabel() {
-	if( window.localStorage.getItem('cpShowHandles') ) {
-		document.body.classList.add('showHandles');
-		buttonLabel = "Hide handles";
-	} else {
-		buttonLabel = "Show handles";
-	}
-}
-
-setHandleButtonLabel();
-
 function renderHandleButton() {
 	
-	var buttonVar = '<div class="handleButton btn submit">'+buttonLabel+'</div>';
-	document.querySelector('#crumbs').insertAdjacentHTML('beforeEnd', buttonVar);
+	if( window.localStorage.getItem('cpShowHandles') ) {
+		document.body.classList.add('showHandles');
+	}
+	
+	var buttonVar = '<div class="handleButton btn submit">Handles</div>';
+	document.querySelector('.btn.submit').insertAdjacentHTML('beforeBegin', buttonVar);
 	
 	var showButton = document.querySelector('.handleButton');
 	
 	showButton.addEventListener('click', function() {
 		document.body.classList.toggle('showHandles');
-		this.textContent = buttonLabel;
 		if( ! window.localStorage.getItem('cpShowHandles') ) {
 			localStorage.setItem('cpShowHandles', true);
 		} else {
 			localStorage.removeItem('cpShowHandles');
 		}
-		setHandleButtonLabel();
 	});
 }
 

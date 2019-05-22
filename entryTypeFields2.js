743276d977b66c36dbd0f040a15b5d98174fa981
Qorridor <coendezeeuw@icloud.com>
2019-05-22 15:17:41 +0200

    Create entryTypeFields2.js

---
+++ entryTypeFields2.js
@@ -0,0 +1,91 @@
+var entryTypeUrls = [];
+var fields = {};
+var responses = [];
+
+fetch(window.Craft.baseCpUrl + '/settings/sections/')
+	  .then(function(response) {
+	    return response.text();
+	  })
+	  .then(function(html) {
+	    var parser = new DOMParser();
+	    var htmlResponse = parser.parseFromString(html, 'text/html');
+	    var entryTypes = htmlResponse.querySelectorAll('#sections tr a[href*="entrytypes/"]');
+	    if (entryTypes.length) {
+	      entryTypes.forEach(function(entryType) {
+	        entryTypeUrls.push([entryType.href]);
+	      });
+	    }
+	  })
+		.then(done => {
+			entryTypeUrls.forEach(entryTypeUrl => {
+				return getEntryTypeFields(entryTypeUrl);
+            });
+        });
+
+function getEntryTypeFields(url) {
+	fetch(url)
+      .then(function(response, url) {
+			responses.push(response.status);
+            return response.text();
+          })
+          .then(function(html) {
+            var parser = new DOMParser();
+            var htmlResponse = parser.parseFromString(html, 'text/html');
+			var handle = htmlResponse.querySelector('#handle');
+            var fieldTabsEl = htmlResponse.querySelectorAll('.fld-tabs .fld-tab');
+			var fieldTabs = {};
+
+			if (fieldTabsEl.length) {
+              fieldTabsEl.forEach(function(fieldTab) {
+
+				var tabFields = [];
+                fieldTabs = {
+					...fieldTabs,
+					[fieldTab.querySelector('.tab span').textContent]: tabFields 
+                }
+				fieldTab.querySelectorAll('.fld-field > span').forEach(field => {
+					tabFields.push(field.title);
+                });
+
+              });
+
+			if(handle) {
+				fields = {
+					...fields,
+                	[handle.value]: fieldTabs
+                }
+    		}
+
+            
+            }
+          })
+		.then(ready => {				
+            responses.length === entryTypeUrls.length && responses.map((response, idx) => {
+                if( response === 200 && idx === entryTypeUrls.length-1 ) {
+                  var responseEl = document.createElement("code"); 
+				  responseEl.style = `
+					position: fixed;
+					padding: 32px;
+					top: 0;
+					z-index: 500;
+					background: black;
+					color: white;
+					max-height: calc(100vh - 64px);
+					overflow: auto;
+					`;
+					Object.entries(fields).map(field => {
+						let fields = [];
+						Object.entries(field[1]).map(data => {
+							fields.push(data);
+						});
+
+						responseEl.innerHTML += `
+							${field[0]}<br>
+							${fields.join('')}<br>
+						`;
+                    });
+                    document.body.appendChild(responseEl); 
+                }
+            });
+    	});
+}
